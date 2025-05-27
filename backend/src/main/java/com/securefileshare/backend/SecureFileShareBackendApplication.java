c47a50bedbaca2cf9b131c3c17725a7774047883
Pranav Ghorpade <pranav.ghorpade3108@gmail.com>
2025-05-27 02:00:42 +0100

    Initial commit – project folder setup

---
+++ backend/src/main/java/com/securefileshare/backend/SecureFileShareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.securefileshare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFileShareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureFileShareBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/securefileshare/backend/SecureFileShareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securefileshare.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureFileShareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
